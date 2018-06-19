589fff6b098e368fc5ab68bfe97edbf44962c50c
Mike Tallerico <mtallerico1@gmail.com>
2018-06-18 20:40:09 -0400

    calls api and appends data from api to page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+const grid = document.querySelector('.grid_wrapper');
+const div = document.createElement('div');
+let seed = '98a517cc24fba74d';
+
+// ------------------------------------------
+//  HELPER FUNCTIONS
+// ------------------------------------------
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
@@ -5,4 +16,37 @@ function fetchData(url) {
     return fetch(url).then(res => res.json());
 }
 
-fetchData('https://randomuser.me/api/?results=12').then(data => console.log(data));
+fetchData(`https://randomuser.me/api/?seed=${seed}&results=12&nat=us&inc=name,location,email,dob,phone,picture`)
+    .then(data => {
+        console.log(data);
+    });
+
+// ------------------------------------------
+//  FETCH FUNCTIONS
+// ------------------------------------------
+
+(function buildCards() {
+    fetchData(`https://randomuser.me/api/?seed=${seed}&results=12&noinfo`)
+    .then(data => {
+        data.results.forEach(element => {
+            const employeeImage = element.picture.large;
+            const employeeFirstName = capitalize(element.name.first);
+            const employeeLastName = capitalize(element.name.last); 
+            const emLocation = capitalize(element.location.city);
+            grid.innerHTML += `<div class="grid_item">
+                <img src="${employeeImage}" class="employee_image">
+                <div class="info_wrapper">
+                    <h3>${employeeFirstName} ${employeeLastName}</h3>
+                    <p>${element.email}</p>
+                    <p>${emLocation}</p>
+                </div>
+            </div>`;
+        });
+    })
+}());
+
+// ------------------------------------------
+//  EVENT LISTENERS
+// ------------------------------------------
+
+
