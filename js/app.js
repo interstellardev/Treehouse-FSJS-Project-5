07cdd0c7e4a9c76a6cbdc60de8221d2c1f4fb1ab
Mike Tallerico <mtallerico1@gmail.com>
2018-06-19 07:43:55 -0400

    modal functionality complete

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
 const grid = document.querySelector('.grid_wrapper');
 const modal = document.querySelector('.modal');
 const div = document.createElement('div');
-let seed = '98a517cc24fba74d';
+const focusCard = document.querySelector('.focus_item');
+let indexCurrent;
+let cardCount;
+let seed = 'e6f1eb12a70145eb';
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
 function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function formatDate(date) {
+    const formattedDate = date.slice(0,10);
+    return formattedDate;
 }
 
 // ------------------------------------------
@@ -17,10 +25,10 @@ function fetchData(url) {
     return fetch(url).then(res => res.json());
 }
 
-fetchData(`https://randomuser.me/api/?seed=${seed}&results=12&nat=us&inc=name,location,email,dob,phone,picture`)
-    .then(data => {
-        console.log(data);
-    });
+// fetchData(`https://randomuser.me/api/?results=12&nat=us&inc=name,location,email,dob,phone,picture`)
+//     .then(data => {
+//         console.log(data);
+//     });
 
 // ------------------------------------------
 //  FETCH FUNCTIONS
@@ -52,10 +60,74 @@ fetchData(`https://randomuser.me/api/?seed=${seed}&results=12&nat=us&inc=name,lo
 //  EVENT LISTENERS
 // ------------------------------------------
 
+//function that creates an event listener for each item in a list
 function createEvent(itemList) {
-    itemList.forEach(element => {
-        element.addEventListener('click', (e) => {
+    for(let i = 0; i < itemList.length; i++) {
+        let employeeCard = itemList[i];
+        employeeCard.addEventListener('click', (e) => {
+            getThisEmployeeData(i);
             modal.style.display = '';
-        }) 
-    });
+        })
+        
+    }
+}
+
+focusCard.addEventListener('click', (e) => {
+    const event = e.target;
+    if(event.id === 'close') {
+        modal.style.display = 'none'
+    }
+    if(event.id === 'left') {
+        getThisEmployeeData((indexCurrent - 1));
+    }
+    if(event.id === 'right') {
+        getThisEmployeeData((indexCurrent + 1));
+    }
+})
+
+function getThisEmployeeData(index) {
+    indexCurrent = index;
+    focusCard.innerHTML = '';
+    fetchData(`https://randomuser.me/api/?seed=${seed}&results=12&noinfo`)
+    .then(data => {
+        const employees = data.results;
+        const employeesImage = employees[index].picture.large;
+        const employeesFirstName = capitalize(employees[index].name.first);
+        const employeesLastName = capitalize(employees[index].name.last);
+        const emsLocation = capitalize(employees[index].location.city);
+        const employeesStreet = employees[index].location.street;
+        const employeesState =  capitalize(employees[index].location.state);
+        const employeesDOB = formatDate(`${employees[index].dob.date}`);
+        console.log(employeesDOB);
+        focusCard.innerHTML += `
+        <div class="focus_item">
+            <img src="img/x-mark.svg" alt="x mark to close window" id="close" display = ''>
+            <img src="img/left_arrow.svg" alt="left arrow" id="left">
+            <img src="img/right_arrow.svg" alt="right arrow" id="right">
+            <div class="image_container">
+                <img src="${employeesImage}" class="employee_image">
+            </div>
+            <h3>${employeesFirstName} ${employeesLastName}</h3>
+            <p>${employees[index].email}</p>
+            <p>${emsLocation}</p>
+            <div class="location_detail">
+                <p>${employees[index].phone}</p>
+                <p>${employeesStreet} ${emsLocation}, ${employeesState}</p>
+                <p>Birthday: ${employeesDOB}</p>
+            </div>
+        </div>
+        `;
+        const leftArrow = document.getElementById('left');
+        const rightArrow = document.getElementById('right');
+        if(indexCurrent === 0) {
+            leftArrow.style.display = 'none';
+        }
+        if(indexCurrent === 11) {
+            rightArrow.style.display = 'none';
+        }
+        });
+}
+
+function search() {
+
 }
